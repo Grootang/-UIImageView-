85b068bea1f21c1d2b65b062e1a4bf272ebf7c58
Grootang <tangzy5578@qq.com>
2017-12-01 10:50:42 +0800

    初始化框架
    1、第一次测试
    2、关于Git
    3、初始化

---
+++ UIImageViewDemo/UIImageViewDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UIImageViewDemo
+//
+//  Created by Grootang on 2017/12/1.
+//  Copyright © 2017年 grootang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UIImageViewDemo/UIImageViewDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UIImageViewDemo
+//
+//  Created by Grootang on 2017/12/1.
+//  Copyright © 2017年 grootang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
